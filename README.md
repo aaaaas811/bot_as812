bc9bb99584c1fc93c239dae6d3abd080c18b9ff0
aaaaas811 <3196611630@qq.com>
2025-12-08 17:16:04 +0800

    虾头/集会更新12.8
    实现了虾头语言检测并发出看看x
    集会相关功能更新

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # bot_as812
 一个基于ncatbot的名为as812的简单QQ聊天bot
+包含插件：
+    Lolicon
+    JmComicPlugin
 
 已实现功能：
     回应戳一戳
     跟贴表情
+    表情歼灭模式（短时间针对单人使用表情刷屏）
+    虾头语言检测
+    mhw/mhr集会码增添、记录
 
 目前待实现功能：
     回应at
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from ncatbot.core import BotClient, PrivateMessage,GroupMessage,NoticeEvent, MessageArray
 from ncatbot.utils import config
 import asyncio
+import re
 # ========== 创建 BotClient ==========
 bot = BotClient()
 # 配置：设置轮询等待时间（秒）
@@ -11,14 +12,21 @@ emoji_kill_model = False
 emoji_kill_times = 8
 emoji_wait_time = 0.1
 emoji_combo={147,127827,127853,10068,76,424,12951,63,66,9992}#废
-# 配置：将这里的账号 ID 替换为你要监控的“账号 MASTER”QQ 号（字符串或整数都可）
-ACCOUNT_MASTER_ID = '3196611630'
+# 配置：虾头语言
+sex_language ={"逼","b","B","批","比","匕","几把","寄吧"}
+# 初始化：集会码
+
+is_mhw_team_code = re.compile(r'^[A-Za-z0-9!#$%&+-=?@[\\\]^_`{|}~]{16}$')
+is_mhr_team_code = re.compile(r'^[A-Za-z0-9!#$%&+-=?@[\\\]^_`{|}~]{12}$')
+mhw=set()
+mhr=set()
+# 配置：将这里的账号 ID 替换为你要监控的“账号 MASTER”QQ 号（字符串或整数都可）#废
 
 # ========= 注册回调函数 ==========
 @bot.private_event()
 async def on_private_message(msg: PrivateMessage):
     global emoji_kill_model
-    if msg.user_id == ACCOUNT_MASTER_ID:
+    if msg.user_id == config.root:
         if msg.raw_message == "测试":
             await bot.api.post_private_msg(msg.user_id, text="NcatBot 测试成功喵~")
         if msg.raw_message == "表情歼灭模式开启":
@@ -30,13 +38,24 @@ async def on_private_message(msg: PrivateMessage):
         if msg.raw_message == "查询表情歼灭模式":
             status = "开启" if emoji_kill_model else "关闭"
             await bot.api.post_private_msg(msg.user_id, text=f"当前表情歼灭模式为：{status} 喵~")
-        
-            await msg.reply(text="诶？我还没搞这个")
 @bot.group_event()
 async def on_group_message(msg: GroupMessage):
     if msg.raw_message == "/菜单":
         menu_text = "诶？我还没搞这个诶"
         await msg.reply(text=menu_text)
+    for x in sex_language:
+        if (x in msg.raw_message) and not (is_mhw_team_code.match(msg.raw_message)) and not (is_mhr_team_code.match(msg.raw_message))==False:
+            await bot.api.post_group_msg(group_id=msg.group_id,text="看看"+x)
+    if is_mhw_team_code.match(msg.raw_message):
+        mhw.add(msg.raw_message)
+        await bot.api.post_group_msg(group_id=msg.group_id,text=f"收到 MHW 集会码：\n{msg.raw_message}\n输入 /查询 获取集会列表喵~") 
+    if is_mhr_team_code.match(msg.raw_message):
+        mhr.add(msg.raw_message)
+        await bot.api.post_group_msg(group_id=msg.group_id,text=f"收到 MHR 集会码：\n{msg.raw_message}\n输入 /查询 获取集会列表喵~") 
+    if msg.raw_message == "/查询":
+        mhw_codes = "\n".join(mhw) if len(mhw) > 0 else "暂无 MHW 集会码"
+        mhr_codes = "\n".join(mhr) if len(mhr) > 0 else "暂无 MHR 集会码"
+        await bot.api.post_group_msg(group_id=msg.group_id,text=f"MHW集会码：\n{mhw_codes}\nMHR 集会码：\n{mhr_codes} ")
 @bot.on_notice() # type: ignore
 async def on_notice(event: NoticeEvent):
     # 兼容不同版本的字段名
@@ -50,7 +69,7 @@ async def on_notice(event: NoticeEvent):
         # 只对账号MASTER的新增表情回应作出反应
         if is_add :
             try:
-                if str(liker) != str(ACCOUNT_MASTER_ID):
+                if str(liker) != str(config.root):
                     return
             except Exception:
                 return
